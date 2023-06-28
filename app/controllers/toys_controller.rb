fccb58c868079edf33c0e557fb1e5cc6d89b5b2f
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-06-28 10:31:29 +0300

    Done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,17 +7,21 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    byebug
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
+    byebug
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    byebug
+    toy = Toy.find_by!(id: params[:id])
     toy.destroy
     head :no_content
   end
@@ -27,5 +31,4 @@ class ToysController < ApplicationController
   def toy_params
     params.permit(:name, :image, :likes)
   end
-
 end
